a6c2a07ae60c26d1a5dfef8b25887288f49d70af
Roman Evtukhov <r.evtukhov@gmail.com>
2022-08-02 22:56:08 +0300

    Initial Commit

---
+++ HW-10 project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-10 project
+//
+//  Created by Roman Evtukhov on 02.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
